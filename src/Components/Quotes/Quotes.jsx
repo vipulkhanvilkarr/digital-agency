6dfcdfa24b21d78977bf247a4928cebc99e5c00f
vipul khanvilkar <110216542+vipulkhanvilkarr@users.noreply.github.com>
2025-03-05 11:55:55 +0530

    first commit

---
+++ src/Components/Quotes/Quotes.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Quotes = () => {
+  return (
+    <>
+      <div className="container py-14 px-4">
+        <h1
+          data-aos="zoom-in"
+          className="text-2xl sm:text-4xl font-bold max-w-[650px] mx-auto text-center "
+        >
+          Building Brands in the Digital Agency World
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default Quotes;
